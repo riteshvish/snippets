5b7bbc8787cba94b03a6bd60ca4ea186b0f27f02
unknown <rtr.riteshvish@gmail.com>
2018-01-27 11:23:14 +0530

    array practice

--- array/index.js
+++ array/index.js
@@ -1,4 +1,5 @@
 console.log("Array");
+// console.log("https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/")
 /*
 var a=[];
 a.copyWithin;
@@ -47,3 +48,23 @@ Array.Array;
 Array.console;
 Array.log;
 */
+var a=[0,1,2,3,4,5,6,7,8];
+var b=[9,10,11,12,13]
+// a.copyWithin(0,4,7) // variable a will change
+// console.log(a.concat(b)); // a will not change
+// console.log(b.concat(a)); // b will not change
+// var a = ['a', 'b', 'c'];
+// var iterator = a.entries();
+// console.log(iterator)
+// for (let e of iterator) {
+//     console.log(e);
+// }
+
+// var array1 = [1, 30, 39, 29, 10, 13];
+
+// console.log(array1.every(function(currentValue) {
+//     console.log(currentValue)
+//     return currentValue < 40;
+// }));
+var a = new Array(3).fill(4);
+console.log(a)
